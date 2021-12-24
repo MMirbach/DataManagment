f5d5e382e26d39777aebcb0d1fafc968828ecc65
MMirbach <70845604+MMirbach@users.noreply.github.com>
2021-12-24 18:12:36 +0200

    part 1 basically works

---
+++ bot.py
@@ -0,0 +1,101 @@
+from telegram.ext import *
+from telegram.update import Update
+from env import API_KEY
+import requests
+
+
+def start_handler(update: Update, context: CallbackContext):
+    update.message.reply_text(f"Hello {update.message.chat.first_name}")
+    update.message.reply_text("Welcome to smart polling.\nPlease choose one of the options:")
+    update.message.reply_text("/register <user-name> - Register to start answering polls via telegram\n"
+                              "<user-name> in smart polling system\n"
+                              "\n"
+                              "/remove <user-name> - To stop getting polls queries\n"
+                              "<user-name> in smart polling system\n"
+                              "\n"
+                              "/start - Use start anytime see get this menu again")
+
+
+def error(update: Update, context: CallbackContext, errorString: str):
+    update.message.reply_text(errorString)
+
+
+def respond(update: Update, context: CallbackContext, httpResponse: requests.Response):
+    update.message.reply_text(httpResponse.text)
+
+def validate_user_name(update: Update, context: CallbackContext):
+    if len(context.args) == 0:
+        error(update, context, "Username cannot be empty")
+        return False
+    elif len(context.args) > 1:
+        error(update, context, "Username cannot contain white spaces")
+        return False
+    return True
+
+def validate_register_user(update: Update, context: CallbackContext):
+    if not validate_user_name(update, context):
+        return False
+    return True
+
+
+def execute_register_user(update: Update, context: CallbackContext):
+    data = {
+        'chat_id': update.message.chat_id,
+        'username': context.args[0]
+    }
+    return requests.post(f"http://localhost:5000/register", data=data)
+
+
+def register_user_handler(update: Update, context: CallbackContext):
+    if not validate_register_user(update, context):
+        return
+    resp = execute_register_user(update, context)
+    respond(update, context, resp)
+
+
+def validate_remove_user(update: Update, context: CallbackContext):
+    if not validate_user_name(update, context):
+        return False
+    return True
+
+
+def execute_remove_user(update: Update, context: CallbackContext):
+    return requests.delete(f"http://localhost:5000/remove/{update.message.chat_id}/{context.args[0]}")
+
+
+def remove_user_handler(update: Update, context: CallbackContext):
+    if not validate_remove_user(update, context):
+        return
+    resp = execute_remove_user(update, context)
+    respond(update, context, resp)
+
+
+def printAnswer(update: Update, context: CallbackContext):
+    # print(update.poll_answer.option_ids[0])  # answer index
+    # print(update.poll_answer.poll_id)  # poll id
+    pass
+
+
+def printUsername(update: Update, context: CallbackContext):
+    try:
+        print(context.user_data['username'])
+    except:
+        print('error')
+
+
+# TODO: check async responses
+
+def main():
+    updater = Updater(API_KEY)
+    dp = updater.dispatcher
+    dp.add_handler(CommandHandler("start", start_handler))
+    dp.add_handler(CommandHandler("register", register_user_handler))
+    dp.add_handler(CommandHandler("remove", remove_user_handler))
+    dp.add_handler(CommandHandler("print", printUsername))
+    dp.add_handler(PollAnswerHandler(printAnswer))
+    updater.start_polling()
+    updater.idle()
+
+
+main()
+
---
+++ server.py
@@ -0,0 +1,84 @@
+import requests, json
+from flask import Flask, request, abort
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import Column, String, Integer, ARRAY, ForeignKey, Table
+from sqlalchemy.exc import IntegrityError
+from sqlalchemy.orm.exc import UnmappedInstanceError
+from env import API_KEY
+
+send_poll_url = f"https://api.telegram.org/bot{API_KEY}/sendPoll"
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://MJ:Pogo97531@localhost/polls'
+db = SQLAlchemy(app)
+
+
+class User(db.Model):
+    __tablename__ = 'users'
+    chat_id = Column(String(64), primary_key=True)
+    username = Column(String(64))
+
+    def __repr__(self):
+        return f"Name: {self.username}, chat: {self.chat_id}"
+
+
+class Poll(db.Model):
+    __tablename__ = 'polls'
+    poll_id = Column(String(64), primary_key=True)
+    poll_question = Column(String(300), nullable=False)
+    poll_answers = Column(ARRAY(String(100)),nullable=False)
+
+
+class Answer(db.Model):
+    __tablename__ = 'answers'
+    chat = Column(String(64), ForeignKey('users.chat_id'), primary_key=True)
+    poll = Column(String(64), ForeignKey('polls.poll_id'), primary_key=True)
+    answer_index = Column(Integer)
+
+
+@app.route('/register', methods=['POST'])
+def register_user():
+    user = User(chat_id=request.form.get('chat_id'), username=request.form.get('username'))
+    try:
+        db.session.add(user)
+        db.session.commit()
+        return f"Successfully registered {request.form.get('username')}"
+    except IntegrityError:
+        abort(409)
+
+
+
+@app.route('/remove/<chat_id>/<username>', methods=['DELETE'])
+def remove_user(chat_id, username):
+    try:
+        user = User.query.filter_by(chat_id=chat_id, username=username).first()
+        db.session.delete(user)
+        db.session.commit()
+        return f"Successfully removed {username}"
+    except UnmappedInstanceError:
+        # this exception is received even if this chat has no user
+        return f"{username} isn't your username"
+
+
+@app.route('/<chat_id>', methods=['GET'])
+def send_poll(chat_id):
+    parameters = {
+        "chat_id" : chat_id,
+        "question" : "What's up?",
+        "options" : json.dumps(["good", "ok", "bad"])
+    }
+    requests.get(send_poll_url, data=parameters)
+
+    return ""
+
+
+@app.errorhandler(409)
+def user_already_exists(error):
+    print(error)
+    return "You already have a user", 409
+
+
+if __name__ == '__main__':
+    db.create_all()
+    app.run(debug=True)
+
