13bddc77b6bf34ac51f9f1bd46ebc9e12c73a350
MMirbach <70845604+MMirbach@users.noreply.github.com>
2022-01-11 10:55:46 +0200

    move charts into component

---
+++ frontend/src/components/Charts.tsx
@@ -0,0 +1,162 @@
+import * as am5 from "@amcharts/amcharts5";
+import * as am5percent from "@amcharts/amcharts5/percent";
+import am5themes_Animated from "@amcharts/amcharts5/themes/Animated";
+import * as am5xy from "@amcharts/amcharts5/xy";
+import { Button } from "@mui/material";
+import axios from "axios";
+import React, { useState, useEffect, useLayoutEffect } from "react";
+
+interface PollData {
+    question: string;
+    answers: Array<{ answer: string; count: number }>;
+}
+
+interface ChartsProps {}
+
+const Charts: React.FC<ChartsProps> = () => {
+    const [polls, setPolls] = useState<Array<PollData>>([]);
+    const [pieCharts, setPieCharts] = useState<boolean>(true);
+
+    const getPolls = async (): Promise<Array<PollData>> => {
+        const res = await axios.get(
+            `http://localhost:${process.env.REACT_APP_SERVER_PORT}/polls`
+        );
+        return res.data;
+    };
+
+    const addPieChart = (
+        root: am5.Root,
+        chartContainer: am5.Container,
+        poll: PollData
+    ): void => {
+        let chart = chartContainer.children.push(
+            am5percent.PieChart.new(root, {
+                radius: 70,
+            })
+        );
+        let numAnswers = poll.answers.reduce((total, answer) => {
+            return total + answer.count;
+        }, 0);
+        chart.children.push(
+            am5.Label.new(root, {
+                text: poll.question + ` (${numAnswers} answers)`,
+                fontSize: 25,
+                x: am5.percent(50),
+                centerX: am5.percent(50),
+            })
+        );
+        let series = chart.series.push(
+            am5percent.PieSeries.new(root, {
+                //name: "Series",
+                categoryField: "answer",
+                valueField: "count",
+                alignLabels: true,
+            })
+        );
+        series.ticks.template.set("visible", false);
+        series.data.setAll(poll.answers);
+    };
+
+    const addXYChart = (
+        root: am5.Root,
+        chartContainer: am5.Container,
+        poll: PollData
+    ): void => {
+        let chart = chartContainer.children.push(
+            am5xy.XYChart.new(root, {
+                width: am5.percent(50),
+            })
+        );
+        let numAnswers = poll.answers.reduce((total, answer) => {
+            return total + answer.count;
+        }, 0);
+        chart.children.push(
+            am5.Label.new(root, {
+                text: poll.question + ` (${numAnswers} answers)`,
+                fontSize: 25,
+                x: am5.percent(50),
+                centerX: am5.percent(50),
+            })
+        );
+        let yAxis = chart.yAxes.push(
+            am5xy.ValueAxis.new(root, {
+                renderer: am5xy.AxisRendererY.new(root, {}),
+            })
+        );
+        let xAxis = chart.xAxes.push(
+            am5xy.CategoryAxis.new(root, {
+                renderer: am5xy.AxisRendererX.new(root, {}),
+                categoryField: "answer",
+            })
+        );
+        xAxis.data.setAll(poll.answers);
+        let series = chart.series.push(
+            am5xy.ColumnSeries.new(root, {
+                xAxis: xAxis,
+                yAxis: yAxis,
+                valueYField: "count",
+                categoryXField: "answer",
+            })
+        );
+        series.columns.template.setAll({
+            width: 50,
+        });
+        series.data.setAll(poll.answers);
+    };
+
+    useLayoutEffect(() => {
+        getPolls().then(res => setPolls(res));
+
+        return () => {};
+    }, [pieCharts]);
+
+    useEffect(() => {
+        let root = am5.Root.new("graphs");
+        root.setThemes([am5themes_Animated.new(root)]);
+        var chartContainer = root.container.children.push(
+            am5.Container.new(root, {
+                layout: root.verticalLayout,
+                width: am5.percent(100),
+                height: am5.percent(100),
+                x: pieCharts ? 0 : am5.percent(25),
+                verticalScrollbar: am5.Scrollbar.new(root, {
+                    orientation: "vertical",
+                }),
+            })
+        );
+
+        polls.map(poll => {
+            pieCharts
+                ? addPieChart(root, chartContainer, poll)
+                : addXYChart(root, chartContainer, poll);
+        });
+        return () => {
+            root.dispose();
+        };
+    }, [polls]);
+
+    const handleToggleChartType = () => {
+        setPieCharts(!pieCharts);
+    };
+
+    return (
+        <React.Fragment>
+            <Button
+                variant="contained"
+                onClick={handleToggleChartType}
+                sx={{ m: 1 }}
+            >
+                Change Chart Type
+            </Button>
+            <div
+                id="graphs"
+                style={{
+                    width: "100%",
+                    height: (polls.length * 45).toString() + "vh",
+                }}
+            ></div>
+        </React.Fragment>
+    );
+};
+
+export default Charts;
--- frontend/src/components/Main.tsx
+++ frontend/src/components/Main.tsx
@@ -1,17 +1,7 @@
-import * as am5 from "@amcharts/amcharts5";
-import * as am5percent from "@amcharts/amcharts5/percent";
-import am5themes_Animated from "@amcharts/amcharts5/themes/Animated";
-import * as am5xy from "@amcharts/amcharts5/xy";
-import { Button } from "@mui/material";
-import axios from "axios";
-import React, { useState, useEffect, useLayoutEffect } from "react";
+import React, { useState } from "react";
+import Charts from "./Charts";
 import Features, { FeatureTypes } from "./Features";
 import Navbar from "./Navbar";
-
-interface PollData {
-    question: string;
-    answers: Array<{ answer: string; count: number }>;
-}
 
 interface MainProps {
     onLogOut: () => void;
@@ -19,126 +9,6 @@ interface MainProps {
 
 const Main: React.FC<MainProps> = ({ onLogOut }) => {
     const [feature, setFeature] = useState<FeatureTypes>(FeatureTypes.None);
-    const [polls, setPolls] = useState<Array<PollData>>([]);
-    const [pieCharts, setPieCharts] = useState<boolean>(true);
-
-    const getPolls = async (): Promise<Array<PollData>> => {
-        const res = await axios.get(
-            `http://localhost:${process.env.REACT_APP_SERVER_PORT}/polls`
-        );
-        return res.data;
-    };
-
-    const addPieChart = (
-        root: am5.Root,
-        chartContainer: am5.Container,
-        poll: PollData
-    ): void => {
-        let chart = chartContainer.children.push(
-            am5percent.PieChart.new(root, {
-                radius: 70,
-            })
-        );
-        let numAnswers = poll.answers.reduce((total, answer) => {
-            return total + answer.count;
-        }, 0);
-        chart.children.push(
-            am5.Label.new(root, {
-                text: poll.question + ` (${numAnswers} answers)`,
-                fontSize: 25,
-                x: am5.percent(50),
-                centerX: am5.percent(50),
-            })
-        );
-        let series = chart.series.push(
-            am5percent.PieSeries.new(root, {
-                //name: "Series",
-                categoryField: "answer",
-                valueField: "count",
-                alignLabels: true,
-            })
-        );
-        series.ticks.template.set("visible", false);
-        series.data.setAll(poll.answers);
-    };
-
-    const addXYChart = (
-        root: am5.Root,
-        chartContainer: am5.Container,
-        poll: PollData
-    ): void => {
-        let chart = chartContainer.children.push(
-            am5xy.XYChart.new(root, {
-                width: am5.percent(50),
-            })
-        );
-        let numAnswers = poll.answers.reduce((total, answer) => {
-            return total + answer.count;
-        }, 0);
-        chart.children.push(
-            am5.Label.new(root, {
-                text: poll.question + ` (${numAnswers} answers)`,
-                fontSize: 25,
-                x: am5.percent(50),
-                centerX: am5.percent(50),
-            })
-        );
-        let yAxis = chart.yAxes.push(
-            am5xy.ValueAxis.new(root, {
-                renderer: am5xy.AxisRendererY.new(root, {}),
-            })
-        );
-        let xAxis = chart.xAxes.push(
-            am5xy.CategoryAxis.new(root, {
-                renderer: am5xy.AxisRendererX.new(root, {}),
-                categoryField: "answer",
-            })
-        );
-        xAxis.data.setAll(poll.answers);
-        let series = chart.series.push(
-            am5xy.ColumnSeries.new(root, {
-                xAxis: xAxis,
-                yAxis: yAxis,
-                valueYField: "count",
-                categoryXField: "answer",
-            })
-        );
-        series.columns.template.setAll({
-            width: 50,
-        });
-        series.data.setAll(poll.answers);
-    };
-
-    useLayoutEffect(() => {
-        getPolls().then(res => setPolls(res));
-
-        return () => {};
-    }, [pieCharts]);
-
-    useEffect(() => {
-        let root = am5.Root.new("graphs");
-        root.setThemes([am5themes_Animated.new(root)]);
-        var chartContainer = root.container.children.push(
-            am5.Container.new(root, {
-                layout: root.verticalLayout,
-                width: am5.percent(100),
-                height: am5.percent(100),
-                x: pieCharts ? 0 : am5.percent(25),
-                verticalScrollbar: am5.Scrollbar.new(root, {
-                    orientation: "vertical",
-                }),
-            })
-        );
-
-        polls.map(poll => {
-            pieCharts
-                ? addPieChart(root, chartContainer, poll)
-                : addXYChart(root, chartContainer, poll);
-        });
-        return () => {
-            root.dispose();
-        };
-    }, [polls]);
 
     const handleShowAdmin = (): void => {
         setFeature(FeatureTypes.ShowAdmins);
@@ -150,10 +20,6 @@ const Main: React.FC<MainProps> = ({ onLogOut }) => {
 
     const handleCreatingPoll = (): void => {
         setFeature(FeatureTypes.CreatePoll);
-    };
-
-    const handleToggleChartType = () => {
-        setPieCharts(!pieCharts);
     };
 
     const handleClose = (): void => {
@@ -169,20 +35,7 @@ const Main: React.FC<MainProps> = ({ onLogOut }) => {
                 onLogOut={onLogOut}
             ></Navbar>
             <Features type={feature} onClose={handleClose}></Features>
-            <Button
-                variant="contained"
-                onClick={handleToggleChartType}
-                sx={{ m: 1 }}
-            >
-                Change Chart Type
-            </Button>
-            <div
-                id="graphs"
-                style={{
-                    width: "100%",
-                    height: (polls.length * 45).toString() + "vh",
-                }}
-            ></div>
+            <Charts></Charts>
         </React.Fragment>
     );
 };
